e47da3989ada838e7d3429e2017c092eb4d401cb
Oleg Pasternak <oleg@minimuminc.com>
2022-09-26 20:41:58 +0300

    Change greeting text by interval

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,12 +1,36 @@
 import styles from './styles.module.scss';
+import { useEffect, useState, useCallback } from 'react';
 
 export default function Hero() {
+  const [title, setTitle] = useState(false);
+  const titles = [
+    'Hello',
+    'Hola',
+    'Olá',
+    'Hei',
+    '여보세요',
+    'γεια',
+    'Kaixo',
+    '你好',
+    'こんにちは',
+	];
+
+	const shuffle = useCallback(() => {
+			const index = Math.floor(Math.random() * titles.length);
+			setTitle(titles[index]);
+	}, []);
+
+	useEffect(() => {
+			const intervalID = setInterval(shuffle, 3000);
+			return () => clearInterval(intervalID);
+	}, [shuffle])
 
   return (
     <div className={styles.hero}>
+			<div className={styles.heroTitle}>
+				{title}
+			</div>
       <h1>
-        Hello!
-        <br />
         I'm Oleg Pasternak
       </h1>
     </div>
