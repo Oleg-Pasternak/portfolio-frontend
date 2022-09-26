62e5174b67a72a254e3c78b7ec83f2621c757fd9
Oleg Pasternak <oleg@minimuminc.com>
2022-09-26 15:41:36 +0300

    Add background blur animation

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,19 @@
 import Header from 'src/components/Header';
+import { useEffect, useState } from 'react';
 import Head from 'src/components/Head';
 
 export default function Home() {
+  const [isLoad, setLoad] = useState(false);
+
+  useEffect(() => {
+    setLoad(true);
+  }, []);
+
   return (
     <body>
       <Head page="メインページ" />
       <Header />
-      <div id='three-js' />
+      <div className={isLoad ? 'page-loaded' : ''} id='three-js' />
       <h1>Hello!
         I'm Oleg Pasternak
       </h1>
