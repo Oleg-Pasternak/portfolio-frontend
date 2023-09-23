197c754fc84210ba5db84bd0e4860134bf8f5c6a
Oleg Pasternak <oleg@minimuminc.com>
2023-09-23 04:14:07 +0300

    Fix animation

--- threejs/three.jsx
+++ threejs/three.jsx
@@ -8,13 +8,15 @@ let isBrowser = typeof window !== "undefined"
 const gradient = new Gradient()
 let isMouseOver = false
 
+let hoveredImageIndex = -1;
+
 //SCROLL
 function raf() {
   let boiler = document.getElementById('boiler')
   let elems = [...document.querySelectorAll('.projects')]
   let objs = Array(3).fill({dist: 0})
   
-  position += speed
+  position -= speed
   speed *= 0.8;
 
   objs.forEach((o,i) => {
@@ -28,7 +30,7 @@ function raf() {
   position += Math.sign(diff)*Math.pow(Math.abs(diff),0.7)*0.015;
 
   // limit position
-  position = Math.max(0, Math.min(2.5, position))
+  position = Math.max(-0.7, Math.min(2.5, position))
 
   var gradientId = document.getElementById('gradient-canvas')
   // check if slide in dataset has changed or dataset.slide does not exist
@@ -72,7 +74,7 @@ export default function Three() {
   cameraGroup.add(camera)
 
   // Renderer
-  const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize( sizes.width, sizes.height );
   renderer.setClearColor( 0x000000, 0 );
   document.getElementById('container').appendChild( renderer.domElement );
@@ -96,7 +98,8 @@ export default function Three() {
         distanceFromCenter: {type: 'f', value: 0},
         uTime: { type: 'f', value: 0 },
         uProgress: { value: 0 },
-        uAlpha: { value: 1 }
+        uAlpha: { value: 1 },
+        uRot: { value: 0.07 }
       },
       vertexShader: `
         varying vec2 vUv;
@@ -104,18 +107,17 @@ export default function Three() {
         uniform float uTime;
         uniform float uProgress;
         uniform float uAlpha;
+        uniform float uRot;
         float PI = 3.14159265358979323846264338;
 
         void main() {
           vUv = (uv - vec2(0.5))*0.8 + vec2(0.5);
           vPosition = position;
           vec3 pos = position;
-          pos.y += sin(PI*vUv.x)*0.07;
-          pos.z += sin(PI*vUv.x)*0.07;
-          // wave
-          // pos.z += sin(position.x * 15.0 + uTime) * 0.04 * (1.0 - uProgress);
-          pos.y += sin(uTime*0.3) * 0.08;
-          vUv.y += sin(uTime*0.3) * 0.05;
+          pos.y += sin(PI*vUv.x)*uRot;
+          pos.z += sin(PI*vUv.x)*uRot;
+          pos.y += sin(uTime*0.3) * uRot;
+          vUv.y += sin(uTime*0.3) * uRot;
           gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
         }
       `,
@@ -131,7 +133,8 @@ export default function Three() {
           vec2 newUV = vUv;
           vec4 color = texture2D(uTexture, newUV);
           gl_FragColor = color;
-          gl_FragColor.a = clamp(distanceFromCenter, 0.2, 1.);
+          vec3 adjustedColor = color.rgb * uAlpha;
+          gl_FragColor = vec4(adjustedColor, clamp(distanceFromCenter, 0.2, 1.));
         }
       `,
       transparent: true
@@ -189,20 +192,22 @@ export default function Three() {
     }
   function onDocumentMouseMove(event) {
       var mouse = new THREE.Vector2();
-      mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-      mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
       var raycaster = new THREE.Raycaster();
-      raycaster.setFromCamera( mouse, camera );
-      var intersects = raycaster.intersectObjects( meshes );
-      var container = document.getElementById('main-container')
-      if(intersects.length > 0) {
-        if(container) {
-          container.style = "cursor:pointer";
+      raycaster.setFromCamera(mouse, camera);
+      var intersects = raycaster.intersectObjects(meshes);
+      var container = document.getElementById('main-container');
+      if (intersects.length > 0) {
+        if (container) {
+          container.style.cursor = "pointer";
+          hoveredImageIndex = meshes.indexOf(intersects[0].object);
           isMouseOver = true
         }
       } else {
-        if(container) {
-          container.style = "cursor:default";
+        if (container) {
+          container.style.cursor = "default";
+          hoveredImageIndex = -1;
           isMouseOver = false
         }
       }
@@ -219,33 +224,33 @@ export default function Three() {
       dist = 1 - dist**2;
       mesh.material.uniforms.uTime.value = performance.now() * 0.001
       mesh.material.uniforms.uProgress.value = dist / 2
-      //mesh.material.uniforms.uAlpha.value = Math.min(Math.abs(position - i),1)
-      //mesh.material.uniforms.uAlpha.value = 1 - mesh.material.uniforms.uAlpha.value**2
       // scale
-      mesh.scale.x = 1 + 0.2*mesh.material.uniforms.uProgress.value
-      mesh.scale.y = 1 + 0.2*mesh.material.uniforms.uProgress.value
-      mesh.scale.z = 1 + 0.2*mesh.material.uniforms.uProgress.value 
+      let scaleValue = 1 + 0.2*mesh.material.uniforms.uProgress.value 
+      mesh.scale.set(scaleValue, scaleValue, scaleValue)
       
       mesh.material.uniforms.distanceFromCenter.value = dist
 
       if (isMouseOver) {
-        // edit geometry of mesh
-        const geometry = geometries[i]
-        const vertices = geometry.attributes.position.array
-        for (let i = 0; i < 121; i += 3) {
-          const x = vertices[i]
-          const y = vertices[i + 1]
-          const z = vertices[i + 2]
-          vertices[i+2] = Math.sin(x * 6 + performance.now() * 0.01) * 0.01 * mesh.material.uniforms.uProgress.value
+        if (i == hoveredImageIndex) {
+          if ( mesh.material.uniforms.uRot.value > 0.06) {
+            mesh.material.uniforms.uRot.value -= 0.001
+          }
+          if (mesh.material.uniforms.uAlpha.value < 1.2) {
+            mesh.material.uniforms.uAlpha.value += 0.01
+          }
         }
-        for (let i = vertices.length; i > vertices.length - 121; i -= 3) {
-          const x = vertices[i]
-          const y = vertices[i + 1]
-          const z = vertices[i + 2]
-          vertices[i+2] = Math.sin(x * 6 + performance.now() * 0.01) * 0.01 * mesh.material.uniforms.uProgress.value
+      } else {
+        if (mesh.material.uniforms.uRot.value < 0.07) {
+          mesh.material.uniforms.uRot.value += 0.001
         }
-        geometry.attributes.position.needsUpdate = true
+        if (mesh.material.uniforms.uAlpha.value > 1) {
+          mesh.material.uniforms.uAlpha.value -= 0.02
+        }
+        mesh.rotation.x = 0
+        mesh.rotation.y = 0
+        mesh.rotation.z = 0
       }
+
 
     })
     
@@ -253,9 +258,9 @@ export default function Three() {
     // get forwar vector of group based on its rotation
     const forward = new THREE.Vector3(0.1,1,0)
     // move group towards forward vector
-    group.position.x = origin_position_x + forward.x * -position * 1.2
-    group.position.y = origin_position_y + forward.y * -position * 1.2
-    group.position.z = origin_position_z + forward.z * -position * 1.2
+    group.position.x = origin_position_x + forward.x * -position * 1.3
+    group.position.y = origin_position_y + forward.y * -position * 1.3
+    group.position.z = origin_position_z + forward.z * -position * 1.3
   }
   animate();
 }
