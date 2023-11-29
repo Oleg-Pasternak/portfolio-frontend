0da6cdbcba78e7ffd270b6fa48545360f43ceb5d
Oleg Pasternak <oleg@minimuminc.com>
2023-11-29 12:23:39 +0200

    Fix deploy

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,26 +8,11 @@ export default function Home() {
 
   const [stocks, setStocks] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://127.0.0.1:5001/api/stocks')
-      .then((res) => {
-        setStocks(res.data)
-      })
-  }, [])
-
   return (
     <div id='main-container'>
       <Head page="Main Page" />
       <Header />
       <Boiler />
-      {stocks && (
-        <ul>
-          {stocks.map((stock) => {
-            return <li>{stock.title} - {stock.price}</li>
-          })}
-        </ul>
-      )}
-      <div id="container" />
     </div>
   )
 }
