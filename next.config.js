f34622dd231125becfc8438d949c6f2e770177f0
Oleg Pasternak <oleg@minimuminc.com>
2023-12-02 05:49:11 +0200

    Fix

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
-// next.config.js
-module.exports = {
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && isServer) {
-      // Enable build caching for production builds on the server
-      config.cache = {
-        type: 'filesystem',
-      };
-    }
-
-    // Add the SCSS/Sass loader configuration
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.(scss|sass)$/,
-        use: [require.resolve('style-loader'), require.resolve('css-loader'), require.resolve('sass-loader')],
-      });
-    }
-
-    return config;
+module.exports = [
+  {
+      extensions: ["scss", "sass"],
+      use: [require.resolve('sass-loader')],
   },
-};
+];
