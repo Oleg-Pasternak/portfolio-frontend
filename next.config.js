bafbd8abcff94043b5ce5ab621e55f591a9e3a7b
Oleg Pasternak <oleg@minimuminc.com>
2023-12-02 05:30:34 +0200

    Fix next.config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
+// next.config.js
 module.exports = {
-  images: {
-    domains: ['res.cloudinary.com'],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.(scss|sass)$/,
+        use: [require.resolve('style-loader'), require.resolve('css-loader'), require.resolve('sass-loader')],
+      });
+    }
+
+    return config;
   },
-  preProcessors: [
-    {
-        extensions: ["scss", "sass"],
-        use: [require.resolve('sass-loader')],
-    },
-  ],
 };
