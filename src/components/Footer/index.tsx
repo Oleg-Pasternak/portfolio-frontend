ee3db917e5a55120adfb45692e5660e79db83d82
Oleg Pasternak <oleg@minimuminc.com>
2023-12-18 00:43:08 +0200

    Reinstall html-react-parser

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,36 +1,22 @@
-import { Parser as HtmlToReactParser } from 'html-to-react';
-
-class HtmlToReactParserWrapper {
-  private parser: any;
-
-  constructor() {
-    this.parser = new HtmlToReactParser();
-  }
-
-  parse(html: string) {
-    return this.parser.parse(html);
-  }
-}
-
-const htmlToReactParser = new HtmlToReactParserWrapper();
+import parse from 'html-react-parser';
 
 const Footer = (props) => {
   return (
     <footer className="footer">
-      <div className="footer-text">
-        {htmlToReactParser.parse(props.footerText)}
-      </div>
-      <div className="footer-inner">
-        <span>© 2023</span>
-        <div className="footer-links">
-          <a href={props.githubLink} target='_blank' rel="noreferrer">
-            <div className="github" />
-          </a>
-          <a href={props.linkedinLink} target='_blank' rel="noreferrer">
-            <div className="linkedin" />
-          </a>
+        <div className="footer-text">
+            {parse(props.footerText)}
         </div>
-      </div>
+        <div className="footer-inner">
+            <span>© 2023</span>
+            <div className="footer-links">
+                <a href={props.githubLink} target='_blank' rel="noreferrer">
+                    <div className="github" />
+                </a>
+                <a href={props.linkedinLink} target='_blank' rel="noreferrer">
+                    <div className="linkedin" />
+                </a>
+            </div>
+        </div>
     </footer>
   );
 };
