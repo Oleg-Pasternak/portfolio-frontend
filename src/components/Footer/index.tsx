b31296927ac5bbd5e806cce465bd905e21617e3e
Oleg Pasternak <oleg@minimuminc.com>
2023-12-17 23:13:11 +0200

    Fix html-parser

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,23 +1,36 @@
 import { Parser as HtmlToReactParser } from 'html-to-react';
-let htmlToReactParser: any = new HtmlToReactParser()
+
+class HtmlToReactParserWrapper {
+  private parser: any;
+
+  constructor() {
+    this.parser = new HtmlToReactParser();
+  }
+
+  parse(html: string) {
+    return this.parser.parse(html);
+  }
+}
+
+const htmlToReactParser = new HtmlToReactParserWrapper();
 
 const Footer = (props) => {
   return (
     <footer className="footer">
-        <div className="footer-text">
-            {htmlToReactParser.parse(props.footerText)}
+      <div className="footer-text">
+        {htmlToReactParser.parse(props.footerText)}
+      </div>
+      <div className="footer-inner">
+        <span>© 2023</span>
+        <div className="footer-links">
+          <a href={props.githubLink} target='_blank' rel="noreferrer">
+            <div className="github" />
+          </a>
+          <a href={props.linkedinLink} target='_blank' rel="noreferrer">
+            <div className="linkedin" />
+          </a>
         </div>
-        <div className="footer-inner">
-            <span>© 2023</span>
-            <div className="footer-links">
-                <a href={props.githubLink} target='_blank' rel="noreferrer">
-                    <div className="github" />
-                </a>
-                <a href={props.linkedinLink} target='_blank' rel="noreferrer">
-                    <div className="linkedin" />
-                </a>
-            </div>
-        </div>
+      </div>
     </footer>
   );
 };
