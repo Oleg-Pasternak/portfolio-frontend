d377e0f8624808f64140de041a0c7c6cd1546507
Oleg Pasternak <oleg@minimuminc.com>
2023-12-18 02:01:18 +0200

    Fix Logo types

--- src/components/ui/Logo/index.tsx
+++ src/components/ui/Logo/index.tsx
@@ -1,9 +1,14 @@
 import { ReactSVG } from "react-svg";
 
-export const Logo = (props) => {
-    return (
-      <div className="logo">
-        <ReactSVG src={props.image} alt="Logo" />
-      </div>
-    );
-  };
+interface LogoProps {
+  image: string;
+  alt: string;
+}
+
+export const Logo = (props: LogoProps) => {
+  return (
+    <div className="logo">
+      <img src={props.image} alt={props.alt} />
+    </div>
+  );
+};
